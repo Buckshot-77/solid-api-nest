6c275ccf2c5f13cb97ff7c1941bc5d0625292410
Hugo Oliveira <hugooliveira.dev@proton.me>
2025-04-08 20:54:18 -0300

    feat: add validation to create user route

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -4,8 +4,22 @@ import {
   Controller,
   HttpCode,
   Post,
+  UsePipes,
 } from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
+
+import { hash } from 'argon2'
+
+import { z } from 'zod'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(10),
+})
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
 
 @Controller('/accounts')
 export class CreateAccountController {
@@ -13,14 +27,17 @@ export class CreateAccountController {
 
   @Post()
   @HttpCode(201)
-  async handle(@Body() body: any) {
-    const { name, email, password: passwordHash } = body
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(@Body() body: CreateAccountBodySchema) {
+    const { name, email, password } = body
 
     const userWithTheSameEmail =
       await this.prismaService.client.user.findUnique({ where: { email } })
 
     if (userWithTheSameEmail)
       throw new ConflictException('There is a user with that email already')
+
+    const passwordHash = await hash(password)
 
     await this.prismaService.client.user.create({
       data: {
---
+++ src/pipes/zod-validation-pipe.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { PipeTransform, BadRequestException } from '@nestjs/common'
+import { ZodError, ZodSchema } from 'zod'
+import { fromZodError } from 'zod-validation-error'
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema<any>) {}
+
+  transform(value: any) {
+    try {
+      this.schema.parse(value)
+    } catch (error) {
+      if (error instanceof ZodError) {
+        throw new BadRequestException({
+          errors: fromZodError(error),
+          message: 'Validation failed',
+          statusCode: 400,
+        })
+      }
+    }
+
+    return value
+  }
+}
