1c121d4f26d60dab98579f69db7978dad7bf2aa0
Hugo Oliveira <hugooliveira.dev@proton.me>
2025-04-15 19:54:07 -0300

    test: add new tests to authenticate student use case

--- src/domain/forum/application/use-cases/authenticate-student.spec.ts
+++ src/domain/forum/application/use-cases/authenticate-student.spec.ts
@@ -3,6 +3,9 @@ import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases
 import { InMemoryStudentsRepository } from '@/test/repositories/in-memory-students-repository'
 import { FakeHasher } from '@/test/cryptography/fake-hasher'
 import { FakeEncrypter } from '@/test/cryptography/fake-encrypter'
+import { WrongCredentialsError } from './errors/wrong-credentials-error'
+import { Student } from '../../enterprise/entities/student'
+import { makeStudent } from '@/test/factories/make-student'
 
 describe('AuthenticateStudent unit tests', () => {
   let authenticateStudentUseCase: AuthenticateStudentUseCase
@@ -28,15 +31,44 @@ describe('AuthenticateStudent unit tests', () => {
       password: 'any-password',
     }
 
+    const student = Student.create(
+      makeStudent({
+        email: payload.email,
+        name: payload.name,
+        passwordHash: payload.password.concat('-hashed'),
+      }),
+    )
+
+    await inMemoryStudentsRepository.create(student)
+
     const result = await authenticateStudentUseCase.execute(payload)
 
-    expect(result.isRight())
+    expect(result.isRight()).toBeTruthy()
     expect(result.value).toEqual({
-      student: expect.objectContaining({
-        email: 'any-email',
-        name: 'any-name',
-        passwordHash: 'any-password-hashed',
-      }),
+      accessToken: expect.any(String),
     })
+  })
+
+  it('should return left, with WrongCredentialsError if student is not found', async () => {
+    const result = await authenticateStudentUseCase.execute({
+      email: 'mail123@mail.com',
+      password: 'any-password',
+    })
+
+    expect(result.isLeft()).toBeTruthy()
+    expect(result.value).toEqual(new WrongCredentialsError())
+  })
+
+  it("should return left, with WrongCredentialsError if student password doesn't match", async () => {
+    const student = Student.create(makeStudent())
+    await inMemoryStudentsRepository.create(student)
+
+    const result = await authenticateStudentUseCase.execute({
+      email: student.email,
+      password: 'another-one-bites-the-dust',
+    })
+
+    expect(result.isLeft()).toBeTruthy()
+    expect(result.value).toEqual(new WrongCredentialsError())
   })
 })
